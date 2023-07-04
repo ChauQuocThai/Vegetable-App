29bc1c5d48a70a7c897ec4af242031430e319a87
ChauQuocThai <chauquocthai10@gmail.com>
2023-07-04 22:35:46 +0700

    first commit

---
+++ OrganicStore/FoodOrderBasicMvpJava/app/src/main/java/com/example/foodorder/constant/Constant.java
@@ -0,0 +1,21 @@
+package com.example.foodorder.constant;
+
+public interface Constant {
+
+    String PAGE_FACEBOOK = "https://www.facebook.com/quoc.thai.33234571";
+    String LINK_FACEBOOK = "https://www.facebook.com/quoc.thai.33234571";
+    String LINK_YOUTUBE = "https://www.youtube.com/@ucAnhNguyenVo";
+    String PHONE_NUMBER = "+84396883228";
+    String GMAIL = "20110398@gmail.com";
+    String SKYPE_ID = "dangtin29932";
+    String ZALO_LINK = "https://zalo.me/0396883228";
+
+    String FIREBASE_URL = "https://organic-store-6dad0-default-rtdb.firebaseio.com";
+
+    String CURRENCY = " 000 VND";
+    int TYPE_PAYMENT_CASH = 1;
+    String PAYMENT_METHOD_CASH = "Tiền mặt";
+
+    // Key Intent
+    String KEY_INTENT_FOOD_OBJECT = "food_object";
+}
